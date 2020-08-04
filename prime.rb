1c41f4b5bec4b29ca8eadb2db8310ff465ad9b5a
Michael Rabon <nobar86@gmail.com>
2020-08-04 18:00:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,7 +2,8 @@ def prime?(num)
   if num <= 1 || num == 0 || num == 1 
     return false
     elsif
-    (2..num - 1).each do |
+    (2..num - 1).each do |i|
+      i % 1 
     return false
 end
 end
